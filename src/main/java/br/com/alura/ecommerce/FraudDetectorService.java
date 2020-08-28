f58502c95f8db3bc1c34bd7a5d18cead4777522a
rodrigobdias <rodrigobdias@gmail.com>
2020-08-28 17:42:12 -0300

    Paralelizando e a importância das keys

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -3,6 +3,7 @@ package br.com.alura.ecommerce;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -50,6 +51,7 @@ public class FraudDetectorService {
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+		properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "_" + UUID.randomUUID().toString());
 
 		return properties;
 	}
--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,6 +1,7 @@
 package br.com.alura.ecommerce;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.producer.Callback;
@@ -13,25 +14,29 @@ public class NewOrderMain {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		var producer = new KafkaProducer<String, String>(properties());
-		
-		var value = "12124, 78754, 878752";
-		var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-		
-		Callback callback = (data, ex) -> {
-			if(ex != null) {
-				ex.printStackTrace();
-				return;
-			}
-			System.out.println("sucesso enviando " + data.topic() + ":::patition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-		};
-		
-		producer.send(record, callback).get();
-		
-		var email = "Bem vindo, estamos processando sua ordem !";
-		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-				
-		producer.send(emailRecord, callback).get();
-		
+
+		for (var i = 0; i < 100; i++) {
+
+			var key = UUID.randomUUID().toString();
+			var value = key + ", 78754, 878752";
+			var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
+
+			Callback callback = (data, ex) -> {
+				if (ex != null) {
+					ex.printStackTrace();
+					return;
+				}
+				System.out.println("sucesso enviando " + data.topic() + ":::patition " + data.partition() + "/ offset "
+						+ data.offset() + "/ timestamp " + data.timestamp());
+			};
+
+			producer.send(record, callback).get();
+
+			var email = "Bem vindo, estamos processando sua ordem !";
+			var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+
+			producer.send(emailRecord, callback).get();
+		}
 	}
 
 	private static Properties properties() {
@@ -39,7 +44,7 @@ public class NewOrderMain {
 		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-				
+
 		return properties;
 	}
 }

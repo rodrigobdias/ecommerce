910d8b3f9772d0d7f0bc4c66d03a3693a8e1dbfa
rodrigobdias <rodrigobdias@gmail.com>
2020-10-16 15:43:48 -0300

    Escolhendo o id adequado

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -38,10 +38,10 @@ public class FraudDetectorService {
 		if(isFraud(order)){
 			// pretending that the fraud happens when the amount is >= 4500
 			System.out.println("Ordem é uma fraude !!!!" + order);
-			orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+			orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order);
 		} else {
 			System.out.println("Aprovado: " + order);
-			orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+			orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order);
 		}
 
 

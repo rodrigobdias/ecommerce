4f90ca0053a7bca76765263b5e6b59b60b7cd319
rodrigobdias <rodrigobdias@gmail.com>
2020-10-22 15:57:17 -0300

    create class 'GenerateAllReportsServlet'

---
+++ service-http-ecommerce/src/main/java/br/com/alura/ecommerce/GenerateAllReportsServlet.java
@@ -0,0 +1,44 @@
+package br.com.alura.ecommerce;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class GenerateAllReportsServlet extends HttpServlet {
+
+    private final KafkaDispatcher<String> batchDispatcher = new KafkaDispatcher<Order>();
+
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        batchDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+
+            batchDispatcher.send("SEND_MESSAGE_TO_ALL_USERS", "USER_GENERATE_READING_REPORT", "USER_GENERATE_READING_REPORT");
+
+            for(User user: users) {
+                batchDispatcher.send("USER_GENERATE_READING_REPORT", user.getUuid(), user);
+            }
+
+            System.out.println("Sent generate reports to all users.");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("Report requests generated");
+
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        }
+
+
+    }
+}

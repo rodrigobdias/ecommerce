d5ac31993f06e9137c6abb6ad8972e2c1bd5c053
rodrigobdias <rodrigobdias@gmail.com>
2020-10-26 21:51:48 -0300

    A serialização customizada com correlation id e um wrapper

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -12,7 +12,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 class KafkaDispatcher<T> implements Closeable {
 
-	private final KafkaProducer<String, T> producer;
+	private final KafkaProducer<String, Message<T>> producer;
 
 	public KafkaDispatcher() {
 		this.producer = new KafkaProducer<>(properties());
@@ -28,7 +28,10 @@ class KafkaDispatcher<T> implements Closeable {
 		return properties;
 	}
 
-	void send(String topic, String key, T value) throws InterruptedException, ExecutionException {
+	void send(String topic, String key, T payload) throws InterruptedException, ExecutionException {
+
+		var value = new Message<>(new CorrelationId(), payload);
+
 		var record = new ProducerRecord<>(topic, key, value);
 
 		Callback callback = (data, ex) -> {

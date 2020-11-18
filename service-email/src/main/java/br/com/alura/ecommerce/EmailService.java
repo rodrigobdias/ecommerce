f7698cd1ed7eb05a187dc609125eadc1295ad624
rodrigobdias <rodrigobdias@gmail.com>
2020-11-18 16:10:37 -0300

    Revisando a arquitetura

--- service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -20,7 +20,7 @@ public class EmailService {
 
 	}
 
-	private void parse(ConsumerRecord<String, String> record) {
+	private void parse(ConsumerRecord<String, Message<String>> record) {
 		System.out.println("---------------------------------------------");
 		System.out.println("Enviando email");
 		System.out.println(record.key());
